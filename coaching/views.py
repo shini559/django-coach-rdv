6f5409db6fe9f08830170f8114fcd0a04dffa0fc
wassim <wassim.motasali@epitech.eu>
2025-07-04 11:56:31 +0200

    :sparkles: Ajout de la homepage

--- coaching/views.py
+++ coaching/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    """
+    Afficher la page d'accueil du site de coaching.
+    :param request:
+    :return:
+    """
+    return render(request, 'coaching/accueil.html')
